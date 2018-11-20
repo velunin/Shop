fdf1f00cec6f74290ac06a322caf0e7ebd06dbc1
Vsevolod Elunin <seva.elunin@gmail.com>
2018-11-20 09:31:26 +0300

    clean

---
+++ Shop.DataAccess/Dto/OrderSaga.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Automatonymous;
+
+namespace Shop.DataAccess.Dto
+{
+    [Table("Shop_OrderSaga")]
+    public class OrderSaga : SagaStateMachineInstance
+    {
+        public OrderSaga(Guid correlationId)
+        {
+            CorrelationId = correlationId;
+        }
+
+        protected OrderSaga()
+        {
+        }
+
+        public Guid CorrelationId { get; set; }
+
+        public string CurrentState { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
