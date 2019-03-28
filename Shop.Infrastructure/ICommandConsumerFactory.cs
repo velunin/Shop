85caf796be016105e08a0523eaa5314ba1d91804
Vsevolod Elunin <seva.elunin@gmail.com>
2019-03-28 16:53:38 +0300

    new service map configuration

---
+++ Shop.Infrastructure/ICommandConsumerFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Shop.Infrastructure
+{
+    public interface ICommandConsumerFactory
+    {
+        object CreateConsumer(Type messageType);
+    }
+
+    public class ServiceCollectionCommandConsumerFactory : ICommandConsumerFactory
+    {
+        public object CreateConsumer(Type messageType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
