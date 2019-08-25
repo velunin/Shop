45309089b2a237922e98e5b56d6ff596f4511726
Vsevolod Elunin <seva.elunin@gmail.com>
2019-08-25 18:48:35 +0300

    fix bugs

--- MassInstance/Bus/ServiceBus.cs
+++ MassInstance/Bus/ServiceBus.cs
@@ -3,7 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using GreenPipes;
-
+using MassInstance.Cqrs.Events;
 using MassTransit;
 using MassTransit.Logging;
 using MassTransit.Topology;
